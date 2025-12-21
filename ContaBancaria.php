e49604e2f3eaeb63885e1c1c2fb00188ed64e96a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-21 07:05:25 -0300

    Criando métodos e utilizando-os

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -6,8 +6,15 @@ class ContaBancaria
     public $nomeTitular = "Gustavo Fraga";
     public $numeroAgencia = 3467;
     public $numeroConta;
-    public $saldo;
+    public $saldo = 1000;
+
+    public function obterSaldo() {
+        return "Seu saldo é: R$ {$this->saldo}";
+    }
 }
 
 $conta = new ContaBancaria();
-var_dump($conta->numeroAgencia);
+var_dump($conta->saldo);
+
+$conta->saldo = 0;
+var_dump($conta->obterSaldo());
