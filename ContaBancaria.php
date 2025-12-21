36541ee86375ab7e86f485a15de4b06014a42e6c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-21 06:52:43 -0300

    Definindo e mostrando atributos

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -3,11 +3,11 @@
 class ContaBancaria 
 {
     public $banco;
-    public $nomeTitular;
-    public $numeroAgencia;
+    public $nomeTitular = "Gustavo Fraga";
+    public $numeroAgencia = 3467;
     public $numeroConta;
     public $saldo;
 }
 
 $conta = new ContaBancaria();
-var_dump($conta);
+var_dump($conta->numeroAgencia);
