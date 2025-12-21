ea160e1237c65d35f36a9c7b658c7909dd1fd425
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-21 10:11:33 -0300

    Implementando os tipos dos atributos e o construtor da classe

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -2,19 +2,30 @@
 
 class ContaBancaria 
 {
-    public $banco;
-    public $nomeTitular = "Gustavo Fraga";
-    public $numeroAgencia = 3467;
-    public $numeroConta;
-    public $saldo = 1000;
+    public string $banco;
+    public string $nomeTitular;
+    public string $numeroAgencia;
+    public string $numeroConta;
+    public float $saldo;
+
+    public function __construct(string $banco, string $nomeTitular, string $numeroAgencia, string $numeroConta, float $saldo)
+    {
+        $this->banco = $banco;
+        $this->nomeTitular = $nomeTitular;
+        $this->numeroAgencia = $numeroAgencia;
+        $this->numeroConta = $numeroConta;
+        $this->saldo = $saldo;
+    }
 
     public function obterSaldo() {
         return "Seu saldo é: R$ {$this->saldo}";
     }
 }
 
-$conta = new ContaBancaria();
-var_dump($conta->saldo);
-
-$conta->saldo = 0;
-var_dump($conta->obterSaldo());
+$conta = new ContaBancaria(
+    'Banco do Brasil',
+    'Gustavo Fraga',
+    '1890',
+    '82.009-5',
+    300.00
+);
